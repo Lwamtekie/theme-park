ab559b7f192e99f1e01bd0216f0335df56b51c08
Lwam Tekie <ltekie87@yahoo.com>
2019-04-02 21:33:24 -0500

    wpi

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log
+// XHR - XmlHttpRequest
+let Rides =[];
+
+const domStringBuilder =(arrayToPrint) => {
+    console.log(arrayToPrint);
+    // loop over arrayToprint and build up domString
+    // call printToDom
+};
+
+
+
+function executeThisCodeAfterFileLoads(){
+    const data = JSON.parse(this.responseText);
+    rides = data.rides;
+    domStringBuilder(data.rides);
+   
+}
+
+
+function executeThisCodeifXHRFAILS(){
+    console.error('oh shit');
+}
+
+
+const getRidesData = () => {
+   const myRequest = new  XMLHttpRequest();
+   myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+   myRequest.addEventListener('error',executeThisCodeifXHRFAILS);
+   myRequest.open('GET', ',/db/rides.json');
+   myRequest.send();
+   console.log(myRequest);
+  
+};
+
+
+function executeThisCodeifXHRFAILS(){
+    console.error('oh shit');
+}
+
+const init= () => {
+    getRidesData();
+};
+init();
